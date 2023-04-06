cd9aa9dd82c47833a0a82d624b326d607d574cb9
Sukrit Saha <sukritsaha27@gmail.com>
2023-04-06 19:00:49 +0530

    adding eye icon for password

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiFillEye, AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 
 interface InputProps {
   placeholder?: string;
@@ -15,16 +16,32 @@ const Input: React.FC<InputProps> = ({
   isDisbaled,
   onChange,
 }) => {
+  const [visible, setVisible] = useState(false);
+
+  const EyeIcon = visible ? AiOutlineEye : AiOutlineEyeInvisible;
+
   return (
-    <input
-      className="w-full text-lg bg-black border-2 border-neutral-800 rounded-md outline-none text-white p-4
-                focus:border-sky-500 focus:border-2 transition disabled:bg-neutral-900 disabled:opacity-70 disabled:cursor-not-allowed"
-      type={type}
-      disabled={isDisbaled}
-      onChange={onChange}
-      value={value ? value : ""}
-      placeholder={placeholder}
-    ></input>
+    <>
+      <div className="relative">
+        <input
+          className="w-full text-lg bg-black border-2 border-neutral-800 rounded-md outline-none text-white p-4
+        focus:border-sky-500 focus:border-2 transition disabled:bg-neutral-900 disabled:opacity-70 disabled:cursor-not-allowed"
+          type={visible ? "text" : type}
+          disabled={isDisbaled}
+          onChange={onChange}
+          value={value ? value : ""}
+          placeholder={placeholder}
+        ></input>
+        {type === "password" && (
+          <div
+            className="text-neutral-500 absolute right-8 top-[1.35rem]"
+            onClick={() => setVisible(!visible)}
+          >
+            <EyeIcon size={23} />
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
