fa6ee324a6af69c2063203b69d19b67c2ce14689
Sukrit Saha <sukritsaha27@gmail.com>
2023-04-14 21:31:21 +0530

    adding motion animation

--- components/LoadingScreen.tsx
+++ components/LoadingScreen.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import { BsTwitter } from "react-icons/bs";
 
 const LoadingScreen = () => {
@@ -12,11 +13,33 @@ const LoadingScreen = () => {
 
   return (
     <>
-      {loading && (
-        <div className="absolute h-full w-full grid place-items-center text-sky-500 bg-black z-50 overflow-hidden">
-          <BsTwitter size={70} />
-        </div>
-      )}
+      <AnimatePresence mode="wait">
+        {loading && (
+          <motion.div
+            initial="initialState"
+            animate="animateState"
+            exit="exitState"
+            variants={{
+              animateState: { opacity: 1 },
+              exitState: { opacity: 0 },
+            }}
+            className="absolute h-full w-full grid place-items-center text-sky-500 bg-black z-50 overflow-hidden"
+          >
+            <motion.div
+              initial="initialState"
+              animate="animateState"
+              exit="exitState"
+              variants={{
+                initialState: { opacity: 0 },
+                animateState: { opacity: 1, y: 5 },
+                exitState: { opacity: 0, y: 0 },
+              }}
+            >
+              <BsTwitter size={70} />
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 };
